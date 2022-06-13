548d6a50a23e5bf453419624432c3b4fab755753
Franck <31331637+franck-boullier@users.noreply.github.com>
2022-06-13 10:49:11 +0800

    Update generate_string.sql
    Minor tweak for clarity and optimization

--- MySQL/scripts-and-random-code/functions/generate_string.sql
+++ MySQL/scripts-and-random-code/functions/generate_string.sql
@@ -8,23 +8,23 @@ DROP FUNCTION IF EXISTS `generate_string`;
 
 # We create the function
 # It will:
-#   - Accept a parameter `length`: a SMALLINT(3)
+#   - Accept a parameter `stringLength`: a SMALLINT(3)
 #   - Return a value as a VARCHAR (100) using the utf8mb4 Character Set.
 DELIMITER $$
-CREATE DEFINER=`root`@`%` FUNCTION `generate_string`(LENGTH SMALLINT(3))
+CREATE DEFINER=`root`@`%` FUNCTION `generate_string`(stringLength SMALLINT(3))
     RETURNS VARCHAR(100) CHARSET utf8mb4
 BEGIN
     # The string is empty at first
     SET @randomString = '';
     # Configure the allowed chars
     SET @allowedChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    # Check the length of the list of Characters we can pick from
+    SET @lenghtAllowedChar = LENGTH(@allowedChars);
     # Start a counter for the loop
     SET @i = 0;
     # Generate a random string and add it to the variable that contains the random string
     # Do this until we have reached the desired length
-    WHILE (@i < LENGTH) DO
-	# Check the length of the list of Characters we can pick from
-	SET @lenghtAllowedChar = LENGTH(@allowedChars);
+    WHILE (@i < stringLength) DO
 	# Select a Random number between 1 and the max length of character list
 	SET @whichtStringNumber = (SELECT FLOOR(1 + RAND()*(@lenghtAllowedChar-1)));
 	# Extract the String we want to add
